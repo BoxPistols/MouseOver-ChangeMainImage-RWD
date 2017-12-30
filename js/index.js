58b7df701563816c3851d83ae959d27bd2a455bd
D.I.J <dgn.fish@gmail.com>
2017-12-31 03:38:51 +0900

    Basic Fix

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
 $(function() {
   var mainimg = $(".mainImage img");
   var list = $(".imageList img");
-  // $(list).on("click", function() {
-  //   $(mainimg).attr("src", $(this).attr("src"));
-  // });
-  $(list).hover(function() {
-    $(mainimg).attr("src", $(this).attr("src"));
+
+  // Set Break Point
+  var w = $(window).width();
+  var x = 768;
+  if (w <= x) {
+    // Mobile
+    $(list).off("mouseenter mouseleave");
+    $(list).on("click", function() {
+      $(mainimg).attr("src", $(this).attr("src"));
+    });
+  } else {
+    // PC
+    $(list).hover(function() {
+      $(mainimg).attr("src", $(this).attr("src"));
+    });
+  };
+
+  // Scroll to Gallery-Top Only Once
+  $(list).one('mouseover', function() {
+    $("html,body").animate({
+      scrollTop: $('.mainImage').offset().top
+    }, 10);
+    $(list).off('mouseover');
   });
+
+  // For Responsive
   var timer = false;
   $(window).resize(function() {
     if (timer !== false) {
@@ -15,17 +35,19 @@ $(function() {
     timer = setTimeout(function() {
       var w = $(window).width();
       var x = 768;
-
       if (w <= x) {
+        // Mobile
         $(list).off("mouseenter mouseleave");
         $(list).on("click", function() {
           $(mainimg).attr("src", $(this).attr("src"));
         });
       } else {
+        // PC
         $(list).hover(function() {
           $(mainimg).attr("src", $(this).attr("src"));
         });
       }
     }, 0);
   });
+
 });
