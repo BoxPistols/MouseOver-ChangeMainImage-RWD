6c142529e6f006ef7d62f672f603c471ad1ce62e
D.I.J <dgn.fish@gmail.com>
2018-01-04 04:20:00 +0900

    Add Files First

--- README.txt
+++
@@ -1,2 +0,0 @@
-if PC : Mouseover change MainImage
-if Mobile : Tap change MainImage
--- js/index.js
+++ js/index.js
@@ -1,53 +1,64 @@
-$(function() {
+$(function () {
   var mainimg = $(".mainImage img");
   var list = $(".imageList img");
-
   // Set Break Point
   var w = $(window).width();
   var x = 768;
   if (w <= x) {
     // Mobile
     $(list).off("mouseenter mouseleave");
-    $(list).on("click", function() {
+    $(list).on("click", function () {
+      $(list).removeClass("on");
+      $(this).addClass("on");
       $(mainimg).attr("src", $(this).attr("src"));
     });
   } else {
     // PC
-    $(list).hover(function() {
-      $(mainimg).attr("src", $(this).attr("src"));
-    });
-  };
+    $(list)
+      .hover(function () {
+        $(list).removeClass("on");
+        $(this).addClass("on");
+        $(mainimg).attr("src", $(this).attr("src"));
+      });
+  }
 
   // Scroll to Gallery-Top Only Once
-  $(list).one('mouseover', function() {
-    $("html,body").animate({
-      scrollTop: $('.mainImage').offset().top
-    }, 10);
-    $(list).off('mouseover');
-  });
+  $(list)
+    .one("mouseover", function () {
+      $("html,body").animate({
+        scrollTop: $(".mainImage")
+          .offset()
+          .top
+      }, 100);
+      $(list).off("mouseover");
+    });
 
   // For Responsive
   var timer = false;
-  $(window).resize(function() {
+  $(window).resize(function () {
     if (timer !== false) {
       clearTimeout(timer);
     }
-    timer = setTimeout(function() {
+    timer = setTimeout(function () {
       var w = $(window).width();
       var x = 768;
       if (w <= x) {
         // Mobile
         $(list).off("mouseenter mouseleave");
-        $(list).on("click", function() {
+        $(list).on("click", function () {
+          $(list).removeClass("on");
+          $(this).addClass("on");
           $(mainimg).attr("src", $(this).attr("src"));
         });
       } else {
         // PC
-        $(list).hover(function() {
-          $(mainimg).attr("src", $(this).attr("src"));
-        });
+        $(list)
+          .hover(function () {
+            $(list).removeClass("on");
+            $(this).addClass("on");
+            $(mainimg).attr("src", $(this).attr("src"));
+          });
       }
     }, 0);
   });
-
 });
